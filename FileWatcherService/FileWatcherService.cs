66f65e2708a5d06d166cb477069dd622d6bc5042
zachstagrammer <32420974+zachstagrammer@users.noreply.github.com>
2024-09-06 16:22:16 -0500

    added FileWatcherService class and initial methods

---
+++ FileWatcherService/FileWatcherService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherService
+{
+    public class FileWatcherService : IFileWatcherService
+    {
+        private readonly FileSystemWatcher _fileWatcher;
+        private readonly ILogger<FileWatcherService> _logger;
+
+        public FileWatcherService(string directoryToWatch, ILogger<FileWatcherService> logger)
+        {
+            _fileWatcher = new FileSystemWatcher(directoryToWatch)
+            {
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName
+            };
+
+            _fileWatcher.Created += OnCreated;
+            _fileWatcher.Changed += OnChanged;
+            _fileWatcher.Deleted += OnDeleted;
+            _fileWatcher.Error += OnError;
+
+            _logger = logger;
+        }
+
+        public void StartWatching()
+        {
+            _fileWatcher.EnableRaisingEvents = true;
+            _logger.LogInformation("File watcher started.");
+        }
+
+        public void StopWatching()
+        {
+            _fileWatcher.EnableRaisingEvents= false;
+            _fileWatcher.Dispose();
+            _logger.LogInformation("File watcher stopped.");
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnError(object sender, ErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
